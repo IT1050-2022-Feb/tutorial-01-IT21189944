76a73c2a21b3d247fff13913fcbcbf4ff487453f
Ishara Madusanka <93942615+IT21189944@users.noreply.github.com>
2022-02-16 12:32:03 +0000

    Tutorial 2 done

--- Tute02.c
+++ Tute02.c
@@ -19,6 +19,22 @@ Amount = 30 x 50 + (50-30) x 40 = 2300*/
 #include <stdio.h>
 
 int main() {
+  int distance, extra = 0;
+  float total = 0;
+
+  printf("Enter the distance that van has travelled : ");
+  scanf("%d", &distance);
+
+  extra = distance - 30; //calculate extra kilometers that van has travelled
+  if (extra <= 0){
+    total = distance * 50;
+    
+  }
+  else{
+    total =  extra * 40 + 30 * 50;
+  }
+
+    printf("Your amount is %.2f", total);
   
   return 0;
 }
